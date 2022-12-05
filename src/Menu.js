54c4143e4d326769318e5112beb75b0a717537ce
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-05 20:33:22 -0300

    Link y NavLink

--- src/Menu.js
+++ src/Menu.js
@@ -1,8 +1,47 @@
 import React from "react";
+import {Link, NavLink} from 'react-router-dom';
 
 function Menu() {
     return (
-        <nav>Menu</nav>
+        <nav>
+          <ul>
+            {/* <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li> */}
+
+            <li>
+              <NavLink
+                style={({isActive}) => ({
+                    color: isActive ? 'red' : 'blue',
+                })} 
+                to='/'
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({isActive}) => ({
+                    color: isActive ? 'red' : 'blue',
+                })} 
+                to='/blog'
+              >Blog</NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({isActive}) => ({
+                    color: isActive ? 'red' : 'blue',
+                })} 
+                to='/profile'
+              >Profile</NavLink>
+            </li>
+
+          </ul>
+        </nav>
     )
 }
 
