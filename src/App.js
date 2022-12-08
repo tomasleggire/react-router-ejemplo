3a10a586eac0b4f4014714f69ca8e785d62eb7c7
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-07 22:07:04 -0300

    Outlet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter, Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import { Menu } from './Menu';
 import { HomePage } from './HomePage';
 import { BlogPage } from './BlogPage';
@@ -19,9 +19,13 @@ function App() {
       <Menu />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/blog' element={<BlogPage />} />
-        <Route path='/blog/:slug' element={<BlogPost />} />
+
+        <Route path='/blog' element={<BlogPage />}>
+          <Route path=':slug' element={<BlogPost />} />
+        </Route>
+
         <Route path='/profile' element={<ProfilePage />} />
+
         <Route path='*' element={<p>Not found</p>} />
       </Routes>
     </HashRouter>
--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,17 +1,20 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {Link, Outlet} from 'react-router-dom';
 import { blogdata } from "./blogdata";
 
 
 function BlogPage() {
     return (
         <>
-        <h1>BlogPage</h1>
-        <ul>
-          {blogdata.map(post => (
-            <BlogLink key={post.slug} post={post}/>
-          ))}
-        </ul>
+          <h1>Blog</h1>
+
+          <Outlet />
+
+          <ul>
+            {blogdata.map(post => (
+              <BlogLink key={post.slug} post={post}/>
+            ))}
+          </ul>
         </>
     )
 }
