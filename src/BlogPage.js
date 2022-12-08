07f6317bdb4441451b2ff715f73245f2ed6fafb0
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-07 21:53:11 -0300

    useNavigate

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -9,7 +9,7 @@ function BlogPage() {
         <h1>BlogPage</h1>
         <ul>
           {blogdata.map(post => (
-            <BlogLink post={post}/>
+            <BlogLink key={post.slug} post={post}/>
           ))}
         </ul>
         </>
