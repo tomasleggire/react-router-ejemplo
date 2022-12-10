a2730564a78a1197a65fbc83df6843e167b2e56f
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-10 19:55:17 -0300

    Menu con rutas publicas y privadas

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,34 @@
 import React from "react";
 import {Link, NavLink} from 'react-router-dom';
+import { useAuth } from "./auth";
 
 function Menu() {
+
+  const auth = useAuth();
+
     return (
         <nav>
           <ul>
+            {routes.map((route) => {
+
+              if (route.private && !auth.user) return null;
+              if (auth.user && route.publicOnly) return null;
+
+              return (
+                <li key={route.to}>
+                  <NavLink
+                    style={({ isActive }) => ({
+                      color: isActive ? 'red' : 'blue',
+                    })}
+                    to={route.to}
+                  >
+                    {route.text}
+                  </NavLink>
+                </li>
+              )
+            })}
+
+
             {/* <li>
               <Link to='/'>Home</Link>
             </li>
@@ -15,7 +39,7 @@ function Menu() {
               <Link to='/profile'>Profile</Link>
             </li> */}
 
-            <li>
+            {/* <li>
               <NavLink
                 style={({isActive}) => ({
                     color: isActive ? 'red' : 'blue',
@@ -54,11 +78,40 @@ function Menu() {
                 })} 
                 to='/logout'
               >Logout</NavLink>
-            </li>
+            </li> */}
 
           </ul>
         </nav>
     )
 }
+
+const routes = [];
+
+routes.push({
+  to: '/',
+  text: 'Home',
+  private: false,
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog',
+  private: false,
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile',
+  private: true,
+});
+routes.push({
+  to: '/login',
+  text: 'Login',
+  private: false,
+  publicOnly: true,
+});
+routes.push({
+  to: '/logout',
+  text: 'Logout',
+  private: true,
+});
 
 export {Menu};
