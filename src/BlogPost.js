ab20f85b70eac0f505cbe118a2fc4e8c7ced6822
Tomás Leggire <tomasleggire@hotmail.com>
2022-12-12 22:17:18 -0300

    Roles y permisos

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link, useParams, useNavigate} from 'react-router-dom';
+import { useAuth } from "./auth";
 import { blogdata } from "./blogdata";
 
 function BlogPost() {
@@ -7,6 +8,10 @@ function BlogPost() {
 
     const { slug } = useParams();
     const blogpost = blogdata.find(post => post.slug === slug);
+
+    const auth = useAuth();
+
+    const canDelete = auth.user?.isAdmin || blogpost.author === auth.user?.username;
 
     const returnToBlog = () => {
       navigate('/blog');
@@ -18,6 +23,11 @@ function BlogPost() {
         <button onClick={returnToBlog}>Volver al blog</button>
         <p>{blogpost.author}</p>
         <p>{blogpost.content}</p>
+
+        {canDelete && (
+          <button>Eliminar blogpost</button>
+        )}
+
         </>
     )
 }
